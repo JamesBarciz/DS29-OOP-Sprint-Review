562d5ce663764f6a73281195f376c18ad6d5d043
James Barciz <jamesjbarciz@gmail.com>
2021-09-02 18:58:24 -0500

    First commit

---
+++ sprint_review/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from ..review import BaseCharacter, Mage
+
+
+@pytest.fixture()
+def template_character():
+    return BaseCharacter('Rico')
+
+
+@pytest.fixture()
+def mage_character():
+    return Mage('Merlin')
